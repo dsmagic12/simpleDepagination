bc37810d6177bb44092bac9620fc361d40e117c5
Daniel Schauer <dschauer12@gmail.com>
2022-01-12 13:35:14 -0800

    combining a paginated HTML table

---
+++ simpleDepagination.js
@@ -0,0 +1,44 @@
+var selTable = "TABLE[summary='LongList']";
+var selPaginationWrapper = "#scriptPagingWPQ1";
+var selPaginationNext = "A[title='Next']";
+
+var oWin = window.open(window.location.href, "_blank", false);
+var oWin2 = window.open("_blank", "_blank", false);
+var bGotPage = false;
+var bWait = true;
+var intvl = null;
+var intvlFirstLoad = setInterval(function(){
+    if ( oWin.document.readyState === "complete" ) {
+        oWin2.document.write(oWin.document.querySelector(selTable).outerHTML);
+        bGotPage = true;
+        var intvlPages = setInterval(function(){
+            
+            if ( bGotPage === true ){
+                if ( oWin.document.querySelector(selPaginationNext) !== null ) {
+                    try{console.log("clicking next page");}catch(e){}
+                    oWin.document.querySelector(selPaginationNext).click();
+                    bGotPage = false;
+                }
+                else {
+                    alert("Done!");
+                    clearInterval(intvlPages);
+                }
+            }
+            bWait = true;
+            intvl = setInterval(function() {
+                if ( oWin.document.readyState === "complete" ) {
+                    try{console.log("next page is loaded");}catch(e){}
+                    bWait = false;
+                    oWin2.document.querySelector(selTable+ " > TBODY").append(oWin.document.querySelector(selTable+ " > TBODY").innerHTML);
+                    bGotPage = true;
+                    clearInterval(intvl);
+                }
+            }, 100);
+        }, 123);
+        try{console.log("first load");}catch(e){}
+        clearInterval(intvlFirstLoad);
+    }
+}, 500);
+
+
+
